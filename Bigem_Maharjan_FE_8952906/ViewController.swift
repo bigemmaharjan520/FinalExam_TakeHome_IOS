637f5c17ed19e7c6a25b4ebc51bbcf10cc14c366
Bigem Maharjan <bigemmaharjan520@gmail.com>
2024-04-02 13:54:17 -0700

    Initial Commit

---
+++ Bigem_Maharjan_FE_8952906/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Bigem_Maharjan_FE_8952906
+//
+//  Created by user240741 on 4/2/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
